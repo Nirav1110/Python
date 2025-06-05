ca4ab25c378ea7045fe6210c79258a28f78036a8
Nirav Parmar <niravparmar@miraiminds.co>
2025-06-06 00:02:19 +0530

    init

---
+++ 1shortcut.py
@@ -0,0 +1,24 @@
+#  
+#python as a calculator
+print(2+3)
+print(4/2) #float division 2.0 (use this)
+print(4//2) #integer division 2
+print(2//4) #integer division 0
+print(2**3) #find a power 8
+print(2**0.5) #gives under root because 1/2=0.5
+print(round(2**0.5),3) #rounds up till 3 after point
+print(3%2) #remainder value (modeulo) 1
+print((2+3)*2)
+print(2**3**2) #assosivity right to left 1st 3**2=9 than 2**9=512
+
+#variables
+a = 2 
+a = 5 #could be updated 
+
+#naming rule
+# can't start with number 1a 
+#can start with _a
+# a1 after string number can given 
+ 
+user_name_one = 123 #snake case writing (used in python)
+userNameOne = 'dkkd' #1st letter small (use in java)
